21cf94f263e047ad8b2dafa5805031c71a5d985a
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-03-29 19:39:25 +0900

    Add post
    post컴포넌트를 구현합니다.

--- src/Post.js
+++ src/Post.js
@@ -19,5 +19,5 @@ export default function Post({ info }){
           {` ${body}`}
         </div>
       </div>
-    );
+    ); 
 }
--- src/api.js
+++ src/api.js
@@ -3,5 +3,5 @@ export async function fetchPostInfos() {
     const response = await fetch(url);
     const data = await response.json();
 
-    return data;
+    return data; 
 }
